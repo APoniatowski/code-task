d9af1542cbabe81210de15c0b1e15c53af8a282b
Adam Poniatowski <adam@poniatowski.dev>
2024-02-11 09:42:16 +0100

    v0.0.3 - graceful shutdown added
    added graceful shutdown

--- pkg/api/run.go
+++ pkg/api/run.go
@@ -1,10 +1,39 @@
 package api
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func (a *App) Run(addr string) {
-	log.Fatal(http.ListenAndServe(addr, a.Router))
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: a.Router,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %s\n", err)
+		}
+	}()
+
+	<-quit
+	log.Println("Shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Error shutting down server: %s\n", err)
+	}
+
+	log.Println("Stopped gracefully")
 }
